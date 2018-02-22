48e152db7d2e55164c5c4262c44abe8f0f629de4
PacPHPdeveloper <shailesh.pacdevelopers@gmail.com>
2018-02-22 16:39:09 +0530

    CkEditor first commit

---
+++ application/views/ckeditor.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script type="text/javascript" src="<?php echo base_url().'assets/ckeditor/ckeditor.js' ?>"></script>
+	<script type="text/javascript" src="<?php echo base_url().'assets/ckeditor/kcfinder/kcfinder.js' ?>"></script>
+</head>
+<body>
+<textarea class="ckeditor" name="ckeditor"></textarea>
+</body>
+</html>
